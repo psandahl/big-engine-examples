146f7095c008d0edc319a0b66487ad18e4ee585a
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-04-14 22:13:24 +0200

    Camera navigation in sky box.

--- cube-map/Main.hs
+++ cube-map/Main.hs
@@ -9,14 +9,15 @@ import           BigE.Runtime
 import           BigE.Texture           (CubeMapFiles (..))
 import qualified BigE.Texture           as Texture
 import           BigE.Types
+import           Control.Monad          (when)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Bits              ((.|.))
 import           Data.Vector.Storable   (Vector, fromList)
 import           Graphics.GL            (GLfloat, GLuint)
 import qualified Graphics.GL            as GL
-import           Linear                 (M44, V3 (..), axisAngle, lookAt,
-                                         mkTransformation, perspective, zero,
-                                         (!*!))
+import           Linear                 (M44, V3 (..), V4 (..), axisAngle,
+                                         lookAt, mkTransformation, normalize,
+                                         perspective, zero, (!*), (!*!))
 
 data State = State
     { program        :: !Program
@@ -26,8 +27,11 @@ data State = State
     , cubeTexture    :: !Texture
     , persp          :: !(M44 GLfloat)
     , view           :: !(M44 GLfloat)
-    , cubeRotation   :: !GLfloat
-    , model          :: !(M44 GLfloat)
+    , viewVector     :: !(V3 GLfloat)
+    , turnLeft       :: !Bool
+    , turnRight      :: !Bool
+    , lookUp         :: !Bool
+    , lookDown       :: !Bool
     } deriving Show
 
 main :: IO ()
@@ -85,9 +89,12 @@ setupCallback = do
                         , mesh = mesh'
                         , cubeTexture = texture
                         , persp = makePerspective width height
-                        , view = lookAt (V3 0 4 10) (V3 0 0 (-1)) (V3 0 1 0)
-                        , cubeRotation = 0
-                        , model = makeRotation 0
+                        , view = lookAt cameraPos startView yAxis
+                        , viewVector = startView
+                        , turnLeft = False
+                        , turnRight = False
+                        , lookUp = False
+                        , lookDown = False
                         }
 
                 Left err -> return $ Left err
@@ -96,12 +103,50 @@ setupCallback = do
 
 animateCallback :: Render State ()
 animateCallback = do
-    duration <- frameDuration
+    duration <- realToFrac <$> frameDuration
     modifyAppState $ \state ->
-        let newRotation = (realToFrac duration) * 0.01 * pi + (cubeRotation state)
-        in state { cubeRotation = newRotation
-                 , model = makeRotation newRotation
-                 }
+        handleLeftTurn duration $
+        handleRightTurn duration $
+        handleLookUp duration $
+        handleLookDown duration state
+
+handleLeftTurn :: GLfloat -> State -> State
+handleLeftTurn duration state
+    | turnLeft state == True =
+        let theta = (realToFrac duration) * 0.5 * pi
+            viewVector' = rotateBy theta (viewVector state)
+            view' = lookAt cameraPos (cameraPos + viewVector') yAxis
+        in state { view = view', viewVector = viewVector' }
+    | otherwise = state
+
+handleRightTurn :: GLfloat -> State -> State
+handleRightTurn duration state
+    | turnRight state == True =
+        let theta = (realToFrac duration) * 0.5 * (-pi)
+            viewVector' = rotateBy theta (viewVector state)
+            view' = lookAt cameraPos (cameraPos + viewVector') yAxis
+        in state { view = view', viewVector = viewVector' }
+    | otherwise = state
+
+handleLookUp :: GLfloat -> State -> State
+handleLookUp duration state
+    | lookUp state == True =
+        let V3 x y z = viewVector state
+            y' = min 1.0 (y + duration)
+            viewVector' = normalize $ V3 x y' z
+            view' = lookAt cameraPos viewVector' yAxis
+        in state { view = view', viewVector = viewVector' }
+    | otherwise = state
+
+handleLookDown :: GLfloat -> State -> State
+handleLookDown duration state
+    | lookDown state == True =
+        let V3 x y z = viewVector state
+            y' = max (-1.0) (y - duration)
+            viewVector' = normalize $ V3 x y' z
+            view' = lookAt cameraPos viewVector' yAxis
+        in state { view = view', viewVector = viewVector' }
+    | otherwise = state
 
 renderCallback :: Render State ()
 renderCallback = do
@@ -112,7 +157,7 @@ renderCallback = do
     Program.enable (program state)
     Mesh.enable (mesh state)
 
-    let mvp = persp state !*! view state !*! model state
+    let mvp = persp state !*! view state
     setUniform (mvpLoc state) mvp
 
     Mesh.render Triangles (mesh state)
@@ -132,13 +177,25 @@ windowSizeCallback width height =
 
 keyPressedCallback :: Key -> ModifierKeys -> Render State ()
 keyPressedCallback key _ = do
-    liftIO $ putStr "Pressed: "
-    liftIO $ putStrLn (show key)
+    when (key == Key'Left) $
+        modifyAppState $ \state -> state { turnLeft = True }
+    when (key == Key'Right) $
+        modifyAppState $ \state -> state { turnRight = True }
+    when (key == Key'Up) $
+        modifyAppState $ \state -> state { lookUp = True }
+    when (key == Key'Down) $
+        modifyAppState $ \state -> state { lookDown = True }
 
 keyReleasedCallback :: Key -> ModifierKeys -> Render State ()
 keyReleasedCallback key _ = do
-    liftIO $ putStr "Released: "
-    liftIO $ putStrLn (show key)
+    when (key == Key'Left) $
+        modifyAppState $ \state -> state { turnLeft = False }
+    when (key == Key'Right) $
+        modifyAppState $ \state -> state { turnRight = False }
+    when (key == Key'Up) $
+        modifyAppState $ \state -> state { lookUp = False }
+    when (key == Key'Down) $
+        modifyAppState $ \state -> state { lookDown = False }
 
 vertices :: GLfloat -> Vector Vertex
 vertices width =
@@ -174,5 +231,20 @@ makePerspective :: Int -> Int -> M44 GLfloat
 makePerspective width height =
     perspective (toRadians 45) (fromIntegral width / fromIntegral height) 0.001 1000
 
+cameraPos :: V3 GLfloat
+cameraPos = V3 0 0 0
+
+startView :: V3 GLfloat
+startView = V3 0 0 (-1)
+
+rotateBy :: GLfloat -> V3 GLfloat -> V3 GLfloat
+rotateBy theta (V3 x y z) =
+    let rotation = makeRotation theta
+        V4 x' y' z' _w = rotation !* V4 x y z 0
+    in normalize $ V3 x' y' z'
+
 makeRotation :: GLfloat -> M44 GLfloat
-makeRotation theta = mkTransformation (axisAngle (V3 0 1 0) theta) zero
+makeRotation theta = mkTransformation (axisAngle yAxis theta) zero
+
+yAxis :: V3 GLfloat
+yAxis = V3 0 1 0
