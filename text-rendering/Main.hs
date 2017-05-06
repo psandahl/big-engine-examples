7b6ab020c954c052cdaf6d6a652696fd28151bd6
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-05-06 17:11:34 +0200

    New stuff from Font. Cleanups.

--- text-rendering/Main.hs
+++ text-rendering/Main.hs
@@ -1,7 +1,5 @@
 module Main where
 
-import           BigE.Attribute.Vert_P  (Vertex (..))
-import           BigE.Mesh              (Mesh)
 import qualified BigE.Mesh              as Mesh
 import qualified BigE.Program           as Program
 import           BigE.Runtime
@@ -9,22 +7,15 @@ import           BigE.TextRenderer.Font (Font)
 import qualified BigE.TextRenderer.Font as Font
 import           BigE.TextRenderer.Text (Text (mesh))
 import qualified BigE.TextRenderer.Text as Text
-import           BigE.Texture           (TextureParameters (..),
-                                         defaultParams2D)
-import qualified BigE.Texture           as Texture
 import           BigE.Types
-import           Data.Either            (isLeft, isRight)
-import           Data.Vector.Storable   (Vector, fromList)
-import           Graphics.GL            (GLfloat, GLint, GLuint)
+import           BigE.Util              (eitherTwo)
+import           Graphics.GL            (GLint)
 import qualified Graphics.GL            as GL
-import           Linear                 (V3 (..))
 
 data State = State
     { program      :: !Program
-    , meshh        :: !Mesh
     , font         :: !Font
     , fontAtlasLoc :: !Location
-    , fontAtlas    :: !Texture
     , text         :: !Text
     } deriving Show
 
@@ -49,28 +40,20 @@ setupCallback = do
                  , (FragmentShader, "text-rendering/fragment.glsl")
                  ]
     eFont <- Font.fromFile "text-rendering/noto-bold.fnt"
-    eFontAtlas <- Texture.fromFile2D "text-rendering/noto-bold.png"
-                defaultParams2D { format = RGBA8
-                                , wrapS = WrapClampToEdge
-                                , wrapT = WrapClampToEdge
-                                }
 
-    case eitherThree (eProg, eFont, eFontAtlas) of
-        Right (prog, font', fontAtlas') -> do
+    case eitherTwo (eProg, eFont) of
+        Right (prog, font') -> do
             fontAtlasLoc' <- Program.getUniformLocation prog "fontAtlas"
 
             GL.glClearColor 0 0 0.4 0
             GL.glEnable GL.GL_BLEND
             GL.glBlendFunc GL.GL_SRC_ALPHA GL.GL_ONE_MINUS_SRC_ALPHA
 
-            meshh' <- Mesh.fromVector StaticDraw vertices indices
             text' <- Text.init font' "Haskell OpenGL Rock!"
 
             return $ Right State
                 { program = prog
-                , meshh = meshh'
                 , font = font'
-                , fontAtlas = fontAtlas'
                 , fontAtlasLoc = fontAtlasLoc'
                 , text = text'
                 }
@@ -85,62 +68,17 @@ renderCallback = do
 
     Program.enable (program state)
     Mesh.enable (mesh $ text state)
-    Texture.enable2D 0 (fontAtlas state)
+    Font.enable 0 (font state)
     setUniform (fontAtlasLoc state) (0 :: GLint)
 
     Mesh.render Triangles (mesh $ text state)
 
-    Texture.disable2D 0
+    Font.disable 0
     Mesh.disable
     Program.disable
 
 teardownCallback :: Render State ()
 teardownCallback = do
     state <- getAppStateUnsafe
-    --Mesh.delete (mesh state)
+    Font.delete (font state)
     Program.delete (program state)
-
-vertices :: Vector Vertex
-vertices =
-    fromList $ quadAt (0, 0) 0.5 0.5
-
-indices :: Vector GLuint
-indices = fromList [0, 1, 2, 0, 2, 3]
-
-quadAt :: (GLfloat, GLfloat) -> GLfloat -> GLfloat -> [Vertex]
-quadAt (leftUpX, leftUpY) width height =
-    [ Vertex { position = V3 (leftUpX + width) leftUpY 0 }
-    , Vertex { position = V3 leftUpX leftUpY 0 }
-    , Vertex { position = V3 leftUpX (leftUpY - height) 0}
-    , Vertex { position = V3 (leftUpX + width) (leftUpY - height) 0}
-    ]
-
-eitherTwo :: (Either a b, Either a c) -> Either a (b, c)
-eitherTwo (e1, e2)
-    | isRight e1 && isRight e2 =
-        let Right e1' = e1
-            Right e2' = e2
-        in Right (e1', e2')
-    | isLeft e1 =
-        let Left err = e1
-        in Left err
-    | otherwise =
-        let Left err = e2
-        in Left err
-
-eitherThree :: (Either a b, Either a c, Either a d) -> Either a (b, c, d)
-eitherThree (e1, e2, e3)
-    | isRight e1 && isRight e2 && isRight e3 =
-        let Right e1' = e1
-            Right e2' = e2
-            Right e3' = e3
-        in Right (e1', e2', e3')
-    | isLeft e1 =
-        let Left err = e1
-        in Left err
-    | isLeft e2 =
-        let Left err = e2
-        in Left err
-    | otherwise =
-        let Left err = e3
-        in Left err
