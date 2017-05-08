b38c879ed75cd2b49d87840f3c65d878ad3df675
Patrik Sandahl <patrik.sandahl@gmail.com>
2017-05-08 17:00:34 +0200

    Non working dynamic text.

--- text-rendering/Main.hs
+++ text-rendering/Main.hs
@@ -4,14 +4,17 @@ import           BigE.Runtime
 import           BigE.TextRenderer      (RenderParams (..), TextRenderer)
 import qualified BigE.TextRenderer      as TextRenderer
 import qualified BigE.TextRenderer.Font as Font
-import           BigE.TextRenderer.Text (Text)
+import           BigE.TextRenderer.Text (Text (..))
 import qualified BigE.TextRenderer.Text as Text
 import           BigE.Util              (eitherTwo)
+import           Control.Monad.IO.Class (liftIO)
 import qualified Graphics.GL            as GL
+import           Text.Printf            (printf)
 
 data State = State
     { textRenderer :: !TextRenderer
     , text         :: !Text
+    , frameCount   :: !Int
     } deriving Show
 
 main :: IO ()
@@ -21,7 +24,7 @@ main = do
                              , displayMode = SizedScreen (1024, 768)
                              , windowCaption = "Text Rendering"
                              , setup = setupCallback
-                             , animate = return ()
+                             , animate = animateCallback
                              , render = renderCallback
                              , teardown = teardownCallback
                              }
@@ -37,18 +40,30 @@ setupCallback = do
         Right (font', textRenderer') -> do
             GL.glClearColor 0 0 0.4 0
 
-            text' <- Text.init font' "Haskell OpenGL Rock!"
+            text' <- Text.init font' "INIT"
 
             return $ Right State
                 { textRenderer = textRenderer'
                 , text = text'
+                , frameCount = 1
                 }
 
         Left err -> return $ Left err
 
+animateCallback :: Render State ()
+animateCallback = do
+    state <- getAppStateUnsafe
+
+    let currCount = frameCount state
+        outStr = printf "Frame #%d" currCount
+    newText <- Text.update outStr (text state)
+
+    putAppState $ state { text = newText, frameCount = currCount + 1 }
+
 renderCallback :: Render State ()
 renderCallback = do
     state <- getAppStateUnsafe
+    liftIO $ putStrLn (string $ text state)
 
     GL.glClear GL.GL_COLOR_BUFFER_BIT
     TextRenderer.render (text state) (RenderParams 20) (textRenderer state)
