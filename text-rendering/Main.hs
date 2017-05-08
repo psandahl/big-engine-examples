13566485dc566fbb6d701824b6aecf61e70eea6f
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-05-08 21:01:10 +0200

    Text rendering.

--- text-rendering/Main.hs
+++ text-rendering/Main.hs
@@ -7,14 +7,14 @@ import qualified BigE.TextRenderer.Font as Font
 import           BigE.TextRenderer.Text (Text (..))
 import qualified BigE.TextRenderer.Text as Text
 import           BigE.Util              (eitherTwo)
-import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad          (when)
 import qualified Graphics.GL            as GL
 import           Text.Printf            (printf)
 
 data State = State
     { textRenderer :: !TextRenderer
     , text         :: !Text
-    , frameCount   :: !Int
+    , fps          :: !Double
     } deriving Show
 
 main :: IO ()
@@ -40,12 +40,13 @@ setupCallback = do
         Right (font', textRenderer') -> do
             GL.glClearColor 0 0 0.4 0
 
-            text' <- Text.init font' "INIT"
+            -- Allocate a text buffer wide enough for updates.
+            text' <- Text.init font' "                          "
 
             return $ Right State
                 { textRenderer = textRenderer'
                 , text = text'
-                , frameCount = 1
+                , fps = 0
                 }
 
         Left err -> return $ Left err
@@ -54,19 +55,19 @@ animateCallback :: Render State ()
 animateCallback = do
     state <- getAppStateUnsafe
 
-    let currCount = frameCount state
-        outStr = printf "Frame #%d" currCount
-    newText <- Text.update outStr (text state)
-
-    putAppState $ state { text = newText, frameCount = currCount + 1 }
+    duration <- frameDuration
+    let newFps = 1.0  / (duration + 0.000001)
+        outStr = printf "FPS: %.2f" newFps
+    when (abs (newFps - fps state) > 0.5) $ do
+        newText <- Text.update outStr (text state)
+        putAppState $ state { text = newText, fps = newFps }
 
 renderCallback :: Render State ()
 renderCallback = do
     state <- getAppStateUnsafe
-    liftIO $ putStrLn (string $ text state)
 
     GL.glClear GL.GL_COLOR_BUFFER_BIT
-    TextRenderer.render (text state) (RenderParams 20) (textRenderer state)
+    TextRenderer.render (text state) (RenderParams 25) (textRenderer state)
 
 teardownCallback :: Render State ()
 teardownCallback = do
